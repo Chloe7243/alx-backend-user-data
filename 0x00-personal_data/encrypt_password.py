51265593e281ef8a366c814183ca8cc375734bc8
Stephanie <Stephaniechloe21@gmail.com>
2024-04-11 19:16:19 +0100

    encrypt fix

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -5,9 +5,9 @@ import bcrypt
 
 def hash_password(password: str) -> bytes:
     """Returns a hashed password"""
-    return bcrypt.hashpw(password, bcrypt.gensalt())
+    return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """Validates that the provided password matches the hashed password"""
-    return bcrypt.checkpw(password, hashed_password)
+    return bcrypt.checkpw(password.encode("utf-8"), hashed_password)
